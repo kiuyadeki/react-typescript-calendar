f0e39f3e19f78092e3346cca0ccb7185418e5953
kiuyadeki <adeki.kiuy.19@gmail.com>
2023-12-17 22:16:22 +0900

    子の数を再帰的に計算

--- src/hooks/useCalculateNodesPosition.tsx
+++ src/hooks/useCalculateNodesPosition.tsx
@@ -3,14 +3,21 @@ import { PersonNodeData, maritalNodeData } from "../types/PersonNodeData";
 
 export function useCalculateNodesPosition(wholeNodes: (PersonNodeData | maritalNodeData)[]) {
   wholeNodes.map(node => {
-    if (node.type === "marital") return;
+    if (node.type === "marital") return node;
+    if (!node.data.children.length) return node;
 
-    if (node.data.children.length) {
-      let numberOfDescendant:number = node.data.children.length;
-      node.data.children.forEach(childId => {
-        let childNode = wholeNodes.find(node => node.id == childId);
-        console.log(node.id, childNode);
-      })
-    }
+    let numberOfDescendant: number = node.data.children.length;
+    node.data.children.forEach(childId => {
+      const childNode = wholeNodes.find(node => node.id === childId);
+
+      // childNodeが無ければreturn;
+      if (!childNode || childNode?.type !== "person") return;
+
+      childNode.data.children.forEach(childId => {
+        numberOfDescendant += childNode.data.children.length; 
+        let grandchildNode = wholeNodes.find(node => node.id === childId);
+      });
+      console.log(node.id, childNode);
+    });
   });
 }
