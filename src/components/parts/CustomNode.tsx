467c100f7fe851ffcd1b224f7648dc75e932bf4b
kiuyadeki <adeki.kiuy.19@gmail.com>
2024-04-21 22:38:18 +0900

    css調整

--- src/components/parts/CustomNode.tsx
+++ src/components/parts/CustomNode.tsx
@@ -7,7 +7,8 @@ import { AnimatePresence, Variants, easeOut, motion } from "framer-motion";
 import { useRecoilValue } from "recoil";
 import { selectedNodeState } from "../../recoil/selectedNodeState";
 import { BiSolidUser } from "react-icons/bi";
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+import { css, keyframes } from "@emotion/react";
 
 export const personNode = (props: NodeProps<PersonData>) => {
   const { id, data } = props;
@@ -15,18 +16,21 @@ export const personNode = (props: NodeProps<PersonData>) => {
   const imageFile = data.profilePicture;
   const isSelected = id === selectedNode?.id;
   const imageURL = imageFile instanceof File ? URL.createObjectURL(imageFile) : undefined;
+  interface StyledBoxProps {
+    isSelected: boolean;
+  }
   const variants: Variants = {
     initial: {
       opacity: 0,
-      // y: -16
+      y: 16,
     },
     animate: {
       opacity: 1,
-      // y: 0
+      y: 0,
     },
     exit: {
       opacity: 0,
-      // y: -16
+      y: 16,
     },
   };
 
@@ -37,47 +41,102 @@ export const personNode = (props: NodeProps<PersonData>) => {
     background: #ccc;
   `;
 
+  const selectedAnimation = keyframes`
+  0% {
+    transform: translate(-50%, -50%) scale(1, 1);
+    // background-color: rgba(144, 205, 244, 0.6);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(1.8, 1.8);
+    // background-color: rgba(144, 205, 244, 0);
+    opacity: 0;
+  }
+  `;
+
+  const selectedStyle = css`
+    &::before {
+      content: "";
+      animation: ${selectedAnimation} 1.5s linear infinite;
+    }
+  `;
+
+  const StyledBox = styled.div<StyledBoxProps>`
+    position: relative;
+    width: ${BASE_PERSON_NODE_WIDTH}px;
+    ${({ isSelected }) => isSelected && selectedStyle};
+    border-radius: 50%;
+    aspect-ratio: 1;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    background-color: #fff;
+    &::before {
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      transition: opacity linear 0.4s;
+      z-index: -1;
+      border: 1px solid #90cdf4;
+    }
+  `;
+
+  const IconBox = styled.div<StyledBoxProps>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 50%;
+    position: relative;
+    background-color: ${({ isSelected }) => (isSelected ? "#3182ce" : "#EB912E")};
+    transform: scale(1.1)
+  `;
+
+  const IconInner = styled.div`
+    position: absolute;
+    inset: 0;
+    border-radius: 50%;
+  `
+
   return (
-    <AnimatePresence>
-      <motion.div
-        key={data.label}
-        variants={variants}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ duration: 0.8, ease: "easeOut" }}
-      >
-        <Box
-          // px={4}
-          // py={2}
-          w={`${BASE_PERSON_NODE_WIDTH}px`}
-          borderRadius="50%"
-          aspectRatio="1"
-          shadow={"md"}
-          position="relative"
+    <>
+      <StyledHandle type="source" position={Position.Right} id="personSourceRight" />
+        <StyledHandle type="source" position={Position.Left} id="personSourceLeft" />
+        <StyledHandle type="source" position={Position.Top} id="personSourceTop" />
+      <AnimatePresence>
+        <motion.div
+          key={data.label}
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ duration: 0.8, ease: "easeOut" }}
         >
-          <StyledHandle type="source" position={Position.Right} id="personSourceRight" />
-          <StyledHandle type="source" position={Position.Left} id="personSourceLeft" />
-          <StyledHandle type="source" position={Position.Top} id="personSourceTop" />
+          <StyledBox isSelected={isSelected}>
+            <IconBox isSelected={isSelected}>
+              <IconInner>
+                {imageURL ? <Image src={imageURL} /> : <BiSolidUser size={100} color="#ffffff" />}
+              </IconInner>
+            </IconBox>
 
-          <Center w="100%" h="100%" overflow="hidden" borderRadius="50%" position="relative" bg={isSelected ? "blue.500" : "white"} transform={"scale(1.1)"}>
-            <Box position="absolute" inset="0" borderRadius="50%" mt={4}>
-              {imageURL ? <Image src={imageURL} /> : <BiSolidUser size={100} color="#ffffff" />}
+            <Box position="absolute">
+              <Text fontSize="md">{id}</Text>
+              <Text fontSize="md">{data.label}</Text>
+              <Text fontSize="md">{data.lastName}</Text>
+              <Text fontSize="md">{data.firstName}</Text>
+              <Text fontSize="md">{data.birthYear}</Text>
+              <Text fontSize="md">{data.birthMonth}</Text>
+              <Text fontSize="md">{data.birthDate}</Text>
+              <Text fontSize="md">{data.gender}</Text>
             </Box>
-          </Center>
-
-          <Box position="absolute">
-            <Text fontSize="md">{id}</Text>
-            <Text fontSize="md">{data.label}</Text>
-            <Text fontSize="md">{data.lastName}</Text>
-            <Text fontSize="md">{data.firstName}</Text>
-            <Text fontSize="md">{data.birthYear}</Text>
-            <Text fontSize="md">{data.birthMonth}</Text>
-            <Text fontSize="md">{data.birthDate}</Text>
-            <Text fontSize="md">{data.gender}</Text>
-          </Box>
-        </Box>
-      </motion.div>
-    </AnimatePresence>
+          </StyledBox>
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 };
